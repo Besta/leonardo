07ef672acdeba0e2b4bf3e213065aa78902a32f7
Simone Bestazza <simone.bestazza@gmail.com>
2016-08-24 20:50:14 +0200

    giorno 2

--- js/base.js
+++ js/base.js
@@ -1,6 +1,40 @@
-$(document).ready(function() {
-    $("#lightgallery").lightGallery({
-        thumbnail:false,
-        selector: ".js-photo"
-    }); 
-});
+/*global $*/
+
+(function () {
+    'use strict';
+    var breakpointMobile = 736;
+    function toggleNav() {
+        var scrollTop = $(document).scrollTop(),
+            offset;
+        if (window.innerWidth > breakpointMobile) {
+            offset = 90;
+        } else {
+            offset = 20;
+        }
+        if (scrollTop > offset) {
+            $('.js-nav').addClass("jscss-active");
+        } else {
+            $('.js-nav').removeClass("jscss-active");
+        }
+    }
+
+    function onScrollHandler(event) {
+        toggleNav();
+    }
+
+    function onResizeHandler(event) {
+        toggleNav();
+    }
+
+    $(document).ready(function () {
+        $(document)
+            .on("scroll", onScrollHandler);
+        $(window)
+            .on("resize", onResizeHandler);
+
+        $("#lightgallery").lightGallery({
+            thumbnail: false,
+            selector: ".js-photo"
+        });
+    });
+}());
